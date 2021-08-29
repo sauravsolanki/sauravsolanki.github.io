1b74e972f48b405e18698ebbb879b8c264829db9
sauravsolanki <ssolanki@deepedge.ai>
2021-08-30 04:18:10 +0530

    intial react layout

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import SiderDemo from "./siderDemo"
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <React.StrictMode>
+    <App/>,
+    // </React.StrictMode>
+    
   document.getElementById('root')
 );
 
